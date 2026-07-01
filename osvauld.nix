e110d6dfc31e10d091a8a8b96aeb9cad51874567
MiracleHuman <lunaregirl@hotmail.com>
2026-07-01 00:43:37 -0400

    Cleaned up osvald.nix

--- osvauld.nix
+++ osvauld.nix
@@ -18,21 +18,20 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-PDuI3Gl1nVrEtKM/di2E2ms3sLvVbFm/IjDWLCXHld8=";
   };
 
-  # nativeBuildInputs are tools executed at build-time
+  
   nativeBuildInputs = [
-    dpkg # Much cleaner for extracting .deb than ar + tar
-    autoPatchelfHook # Automatically fixes missing libraries!
-    wrapGAppsHook3 # Wraps the binary to set up GTK schemas
+    dpkg 
+    autoPatchelfHook 
+    wrapGAppsHook3
   ];
 
-  # buildInputs are the libraries your app links against at runtime
+  
   buildInputs = [
     gtk3
     webkitgtk_4_1
   ];
 
   unpackPhase = ''
-    # Cleanly unpacks the deb directly into the build directory
     dpkg-deb -x $src .
   '';
 
@@ -41,17 +40,13 @@ stdenv.mkDerivation rec {
 
     mkdir -p $out
 
-    # Copy extracted contents into the Nix store output
     cp -r usr/bin $out/bin
     cp -r usr/share $out/share
 
-    # Fix absolute paths in the .desktop file
     substituteInPlace $out/share/applications/osvauld.desktop \
       --replace-fail 'Exec=livnote' "Exec=$out/bin/livnote"
 
     runHook postInstall
   '';
 
-  # Note: autoPatchelfHook runs automatically in the fixupPhase,
-  # so you don't need a custom preFixup or patchelf commands!
 }
